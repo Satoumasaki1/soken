76e6193c9efba75306f361c84be818dfa455ecb7
Waki_Furio <marvel.2003.waki@gmail.com>
2025-01-16 13:51:46 +0900

    プレハブにタグを設定

--- TowerDEF/Assets/Script/SeasonObjectManager.cs
+++ TowerDEF/Assets/Script/SeasonObjectManager.cs
@@ -5,21 +5,42 @@ public class SeasonObjectManager : MonoBehaviour
     // 対応するタグ名
     private readonly string[] seasonTags = { "Spring", "Summer", "Autumn", "Winter" };
 
+    private GameManager.Season currentSeasonCache;
+
+    private void Start()
+    {
+        // 初期状態を設定
+        UpdateSeasonObjects(GameManager.Instance.currentSeason);
+    }
+
     private void Update()
     {
         // GameManager のインスタンスが存在するか確認
         if (GameManager.Instance != null)
         {
-            // 現在のシーズンを取得
             GameManager.Season currentSeason = GameManager.Instance.currentSeason;
 
-            // 全てのタグについて処理を行う
-            foreach (string tag in seasonTags)
+            // 現在の季節が変更された場合のみ処理を行う
+            if (currentSeason != currentSeasonCache)
             {
-                // 現在のシーズンと一致するタグは有効化、一致しないタグは無効化
-                bool shouldActivate = tag == currentSeason.ToString();
-                SetObjectsActiveByTag(tag, shouldActivate);
+                currentSeasonCache = currentSeason;
+                UpdateSeasonObjects(currentSeason);
             }
+        }
+    }
+
+    /// <summary>
+    /// 現在の季節に対応するオブジェクトを有効化し、それ以外を無効化する
+    /// </summary>
+    /// <param name="currentSeason">現在の季節</param>
+    private void UpdateSeasonObjects(GameManager.Season currentSeason)
+    {
+        // 全てのタグについて処理を行う
+        foreach (string tag in seasonTags)
+        {
+            // 現在のシーズンと一致するタグは有効化、一致しないタグは無効化
+            bool shouldActivate = tag == currentSeason.ToString();
+            SetObjectsActiveByTag(tag, shouldActivate);
         }
     }
 
